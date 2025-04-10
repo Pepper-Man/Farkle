d96457acb8f67ff6e6d5ccd85b4f6ec53ba9e5bb
Jake Boraman <jake.boraman@gmail.com>
2025-04-10 16:45:53 +0100

    Add score/pass buttons to UI, refactor dice throwing code into own function

--- FarkleUI/MyForm.h
+++ FarkleUI/MyForm.h
@@ -24,6 +24,9 @@ namespace FarkleUI {
 			InitializeComponent();
 			srand(time(NULL));
 			dynamicButtons = gcnew System::Collections::Generic::List<System::Windows::Forms::Button^>();
+			throwButton->Enabled = true;
+			againButton->Enabled = false;
+			passButton->Enabled = false;
 		}
 
 	protected:
@@ -40,10 +43,26 @@ namespace FarkleUI {
 	private: System::Windows::Forms::Button^ throwButton;
 	protected:
 
-	private: System::Windows::Forms::Label^ statusLabel;
+	protected:
+
+	protected:
+
+
 		   System::Collections::Generic::List<System::Windows::Forms::Button^>^ dynamicButtons;
 		   System::Drawing::Rectangle allowedArea;
 	private: System::Windows::Forms::Panel^ diceArea;
+	private: System::Windows::Forms::Button^ againButton;
+
+
+	private: System::Windows::Forms::Button^ passButton;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+
+
 
 
 	protected:
@@ -62,8 +81,15 @@ namespace FarkleUI {
 		void InitializeComponent(void)
 		{
 			this->throwButton = (gcnew System::Windows::Forms::Button());
-			this->statusLabel = (gcnew System::Windows::Forms::Label());
 			this->diceArea = (gcnew System::Windows::Forms::Panel());
+			this->againButton = (gcnew System::Windows::Forms::Button());
+			this->passButton = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// throwButton
@@ -72,52 +98,148 @@ namespace FarkleUI {
 			this->throwButton->BackColor = System::Drawing::SystemColors::Control;
 			this->throwButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->throwButton->Location = System::Drawing::Point(325, 62);
+			this->throwButton->Location = System::Drawing::Point(12, 12);
 			this->throwButton->Name = L"throwButton";
 			this->throwButton->Size = System::Drawing::Size(125, 60);
 			this->throwButton->TabIndex = 0;
 			this->throwButton->Text = L"Throw dice!";
 			this->throwButton->UseVisualStyleBackColor = false;
-			this->throwButton->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// statusLabel
-			// 
-			this->statusLabel->Location = System::Drawing::Point(325, 9);
-			this->statusLabel->Name = L"statusLabel";
-			this->statusLabel->Size = System::Drawing::Size(125, 16);
-			this->statusLabel->TabIndex = 1;
-			this->statusLabel->Text = L"status label";
-			this->statusLabel->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->throwButton->Click += gcnew System::EventHandler(this, &MyForm::diceThrower);
 			// 
 			// diceArea
 			// 
 			this->diceArea->BackColor = System::Drawing::Color::Transparent;
 			this->diceArea->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->diceArea->ForeColor = System::Drawing::Color::Transparent;
-			this->diceArea->Location = System::Drawing::Point(12, 152);
+			this->diceArea->Location = System::Drawing::Point(12, 217);
 			this->diceArea->Name = L"diceArea";
-			this->diceArea->Size = System::Drawing::Size(758, 589);
+			this->diceArea->Size = System::Drawing::Size(758, 536);
 			this->diceArea->TabIndex = 2;
+			// 
+			// againButton
+			// 
+			this->againButton->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->againButton->BackColor = System::Drawing::SystemColors::Control;
+			this->againButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->againButton->Location = System::Drawing::Point(325, 12);
+			this->againButton->Name = L"againButton";
+			this->againButton->Size = System::Drawing::Size(125, 60);
+			this->againButton->TabIndex = 3;
+			this->againButton->Text = L"Score and roll again";
+			this->againButton->UseVisualStyleBackColor = false;
+			// 
+			// passButton
+			// 
+			this->passButton->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->passButton->BackColor = System::Drawing::SystemColors::Control;
+			this->passButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->passButton->Location = System::Drawing::Point(645, 12);
+			this->passButton->Name = L"passButton";
+			this->passButton->Size = System::Drawing::Size(125, 60);
+			this->passButton->TabIndex = 5;
+			this->passButton->Text = L"Score and pass";
+			this->passButton->UseVisualStyleBackColor = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(321, 75);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(97, 20);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Total/4000";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(432, 95);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(18, 20);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"0";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(321, 115);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(94, 20);
+			this->label3->TabIndex = 8;
+			this->label3->Text = L"This Round";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(432, 135);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(18, 20);
+			this->label4->TabIndex = 9;
+			this->label4->Text = L"0";
+			this->label4->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(321, 155);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(74, 20);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"Selected";
+			this->label5->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(432, 175);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(18, 20);
+			this->label6->TabIndex = 11;
+			this->label6->Text = L"0";
+			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(782, 753);
+			this->ClientSize = System::Drawing::Size(782, 765);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->passButton);
+			this->Controls->Add(this->againButton);
 			this->Controls->Add(this->diceArea);
-			this->Controls->Add(this->statusLabel);
 			this->Controls->Add(this->throwButton);
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Farkle";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		// Update the label text
-		statusLabel->Text = "Button clicked!!";
 
+	private: System::Void diceThrower(System::Object^ sender, System::EventArgs^ e)
+	{
 		// Remove all existing dynamic buttons
 		for each (Button ^ btn in dynamicButtons) {
 			diceArea->Controls->Remove(btn);
@@ -181,29 +303,35 @@ namespace FarkleUI {
 			newButton->Click += gcnew EventHandler(this, &MyForm::DynamicButton_Click);
 
 			dynamicButtons->Add(newButton);
+
+			// Update control buttons
+			throwButton->Enabled = false;
+			againButton->Enabled = true;
+			passButton->Enabled = true;
 		}
 	}
-		   System::Void DynamicButton_Click(System::Object^ sender, System::EventArgs^ e) {
-			   Button^ clickedButton = (Button^)sender;
 
-			   /* Test code
-			   * MessageBox::Show(String::Format("You clicked {0} at position ({1}, {2})",
-				   clickedButton->Name,
-				   clickedButton->Location.X,
-				   clickedButton->Location.Y));
-			   */
+	System::Void DynamicButton_Click(System::Object^ sender, System::EventArgs^ e) {
+		Button^ clickedButton = (Button^)sender;
 
-			   // Set border if adding to selection, else remove border to deselect
-			   if (clickedButton->FlatAppearance->BorderSize == 0)
-			   {
-				   clickedButton->FlatAppearance->BorderSize = 3;
-				   clickedButton->FlatAppearance->BorderColor = Color::LimeGreen;
-			   }
-			   else
-			   {
-				   clickedButton->FlatAppearance->BorderSize = 0;
-			   }
-		   }
+		/* Test code
+		* MessageBox::Show(String::Format("You clicked {0} at position ({1}, {2})",
+			clickedButton->Name,
+			clickedButton->Location.X,
+			clickedButton->Location.Y));
+		*/
+
+		// Set border if adding to selection, else remove border to deselect
+		if (clickedButton->FlatAppearance->BorderSize == 0)
+		{
+			clickedButton->FlatAppearance->BorderSize = 3;
+			clickedButton->FlatAppearance->BorderColor = Color::LimeGreen;
+		}
+		else
+		{
+			clickedButton->FlatAppearance->BorderSize = 0;
+		}
+	}
 
 	// Determine if new button (die) location intersects with any existing button (die)
 	private: bool IsOverlapping(Button^ newButton, Point proposedLocation)
